b7160dc78ca841fab0e611e5c29d893f969dfa43
Devarat Patel <devaratpatel1@gmail.com>
2022-07-31 19:37:14 -0400

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
-        <img src={Logo} alt='Logo Image' style={{ width: '140px', marginTop: '50px' }} />
+        <img src={Logo} alt='Logo Image' style={{ width: '120px', marginTop: '40px' }} />
       </div>
 
       {/* menu */}
